9a88dd0b0d4651f04beee26751a02c59ba1f300a
Bryce <bryceandy@rocketmail.com>
2018-06-26 03:04:48 +0300

    landing

--- resources/views/templates/application.blade.php
+++ resources/views/templates/application.blade.php
@@ -30,6 +30,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/animation.gsap.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
     @yield('scripts')
     <script type="text/javascript" src="{{'js/main.js'}}"></script>
