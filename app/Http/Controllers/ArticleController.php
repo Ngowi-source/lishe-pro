75d20a9456198f56492fbe56b4193e9a1d88db6b
Bryce <bryceandy@rocketmail.com>
2018-07-03 12:00:05 +0300

    blog

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -32,7 +32,7 @@ class ArticleController extends Controller
 
         $articles = $articles->simplePaginate(3);
 
-        $archives = Article::selectRaw('YYYY(created_at) year, monthname(created_at) month, count(*) published')->groupBy('year', 'month')->orderByRaw('min(created_at) desc')->get()->toArray();
+        $archives = Article::selectRaw('YYYY(created_at) as year, month(created_at) as month, count(*) published')->groupBy('year', 'month')->orderByRaw('min(created_at) desc')->get()->toArray();
 
         return view('blogs.index', compact('articles'/*, 'archives'*/));
     }
