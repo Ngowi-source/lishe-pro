aca5ee912381f1e328f628913451800388e2b1f2
Bryce <bryceandy@rocketmail.com>
2018-07-11 00:41:29 +0300

    vhnages

--- resources/views/blogs/create.blade.php
+++ resources/views/blogs/create.blade.php
@@ -2,8 +2,8 @@
 
 @section('stylesheets')
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/js/bootstrap.min.js"></script>
 
