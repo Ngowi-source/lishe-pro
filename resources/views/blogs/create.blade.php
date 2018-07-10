7a7379de9c600ea7de5fbc4a1a52801df2c03d60
Bryce <bryceandy@rocketmail.com>
2018-07-11 01:03:22 +0300

    changes

--- resources/views/blogs/create.blade.php
+++ resources/views/blogs/create.blade.php
@@ -2,7 +2,6 @@
 
 @section('stylesheets')
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/js/bootstrap.min.js"></script>
