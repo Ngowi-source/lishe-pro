2d69710020ded00a87c009a0c7493963db0f2cf7
Bryce <bryceandy@rocketmail.com>
2018-07-28 22:51:06 +0300

    changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -159,7 +159,7 @@
                             <h2><a href="/blog/{{str_replace(' ','-',$articles[1]->title)}}">{{$articles[1]->title}}</a></h2>
                             <span class="articleTime">{{$articles[1]->created_at->toFormattedDateString()}} by <b>{{$articles[1]->user->firstname}} {{$articles[1]->user->lastname}}</b> </span><br /><br />
 
-                            <span class="articleBod">@if(strlen($articles[1])>130){!! substr($articles[1]->body,0,130) !!}... <span class="readMore"><a href="/blog/{{str_replace(' ','-',$articles[1]->title)}}"><i>Read More</i></a></span> @else{!! $articles[1]->body !!}@endif</span><br />
+                            <span class="articleBod">@if(strlen($articles[1])>125){!! substr($articles[1]->body,0,125) !!}... <span class="readMore"><a href="/blog/{{str_replace(' ','-',$articles[1]->title)}}"><i>Read More</i></a></span> @else{!! $articles[1]->body !!}@endif</span><br />
                             <span class="articleTime"><i>{{count($articles[1]->comments)}} comments</i></span>
                         </div>
                     </div>&nbsp;
@@ -171,7 +171,7 @@
                             <h2><a href="/blog/{{str_replace(' ','-',$articles[2]->title)}}">{{$articles[2]->title}}</a></h2>
                             <span class="articleTime">{{$articles[2]->created_at->toFormattedDateString()}} by <b>{{$articles[2]->user->firstname}} {{$articles[2]->user->lastname}}</b> </span><br /><br />
 
-                            <span class="articleBod">@if(strlen($articles[2])>130){!! substr($articles[2]->body,0,130) !!}... <span class="readMore"><a href="/blog/{{str_replace(' ','-',$articles[2]->title)}}"><i>Read More</i></a></span> @else{!! $articles[2]->body !!}@endif</span><br />
+                            <span class="articleBod">@if(strlen($articles[2])>125){!! substr($articles[2]->body,0,125) !!}... <span class="readMore"><a href="/blog/{{str_replace(' ','-',$articles[2]->title)}}"><i>Read More</i></a></span> @else{!! $articles[2]->body !!}@endif</span><br />
                             <span class="articleTime"><i>{{count($articles[2]->comments)}} comments</i></span>
                         </div>
                     </div>
