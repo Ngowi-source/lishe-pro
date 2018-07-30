241a61e8ada959b6d9b679e5c1cf5b35375ac6d8
Bryce <bryceandy@rocketmail.com>
2018-07-30 14:57:11 +0300

    editor

--- public/js/main.js
+++ public/js/main.js
@@ -18,15 +18,15 @@ new ScrollMagic.Scene({
     triggerHook: "onEnter",
     reverse: false
 })
-    .setTween(TweenMax.from("#whoAreWe h1", 0.6, {x: -600, autoAlpha: 0.0, ease: Power0.easeIn}))
+    .setTween(TweenMax.from("#whoAreWe h1", 0.6, {x: -600, autoAlpha: 0.0, ease: Power2.easeIn}))
     .addTo(controller);
 
 new ScrollMagic.Scene({
-    triggerElement: "#whoAreWe h1",
+    triggerElement: ".whoAreWe",
     triggerHook: "onEnter",
     reverse: false
 })
-    .setTween(TweenMax.from(".whoAreWe", 0.8, {y: -400, autoAlpha: 0.0, ease: Power1.easeIn}))
+    .setTween(TweenMax.from(".whoAreWe", 0.8, {y: -400, autoAlpha: 0.0, ease: Power2.easeIn}))
     .addTo(controller);
 
 new ScrollMagic.Scene({
@@ -34,7 +34,7 @@ new ScrollMagic.Scene({
     triggerHook: "onEnter",
     reverse: false
 })
-    .setTween(TweenMax.from("#wlt", 0.6, {y: -350, autoAlpha: 0.0, ease: Power0.easeIn}))
+    .setTween(TweenMax.from("#wlt i", 0.6, {y: -350, autoAlpha: 0.0, ease: Power0.easeIn}))
     .addTo(controller);
 
 new ScrollMagic.Scene({
@@ -42,7 +42,7 @@ new ScrollMagic.Scene({
     triggerHook: "onEnter",
     reverse: false
 })
-    .setTween(TweenMax.from("#dmp", 0.6, {y: -350, autoAlpha: 0.0, ease: Power0.easeIn}))
+    .setTween(TweenMax.from("#dmp i", 0.6, {y: -350, autoAlpha: 0.0, ease: Power0.easeIn}))
     .addTo(controller);
 
 new ScrollMagic.Scene({
@@ -50,7 +50,7 @@ new ScrollMagic.Scene({
     triggerHook: "onEnter",
     reverse: false
 })
-    .setTween(TweenMax.from("#ofd", 0.6, {y: -350, autoAlpha: 0.0, ease: Power0.easeIn}))
+    .setTween(TweenMax.from("#ofd i", 0.6, {y: -350, autoAlpha: 0.0, ease: Power0.easeIn}))
     .addTo(controller);
 
 new ScrollMagic.Scene({
@@ -58,7 +58,7 @@ new ScrollMagic.Scene({
     triggerHook: "onEnter",
     reverse: false
 })
-    .setTween(TweenMax.from("#bmi", 0.6, {y: -350, autoAlpha: 0.0, ease: Power0.easeIn}))
+    .setTween(TweenMax.from("#bmi i", 0.6, {y: -350, autoAlpha: 0.0, ease: Power0.easeIn}))
     .addTo(controller);
 
 new ScrollMagic.Scene({
@@ -66,7 +66,7 @@ new ScrollMagic.Scene({
     triggerHook: "onEnter",
     reverse: false
 })
-    .setTween(TweenMax.from("#fcc", 0.6, {y: -350, autoAlpha: 0.0, ease: Power0.easeIn}))
+    .setTween(TweenMax.from("#fcc i", 0.6, {y: -350, autoAlpha: 0.0, ease: Power0.easeIn}))
     .addTo(controller);
 
 new ScrollMagic.Scene({
@@ -75,5 +75,5 @@ new ScrollMagic.Scene({
     offset: 200,
     reverse: false
 })
-    .setTween(TweenMax.from("#blog .card", 0.7, {y: -350, scale: 0.6, autoAlpha: 0.0, ease: Power0.easeIn}))
+    .setTween(TweenMax.from("#blog .card", 0.7, {y: 350, scale: 0.6, autoAlpha: 0.0, ease: Power0.easeIn}))
     .addTo(controller);
