ea7062c3701f4da7838257b699f0594a26165665
Bryce <bryceandy@rocketmail.com>
2018-08-03 21:23:52 +0300

    commit

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -18,4 +18,9 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function archivesTest()
+    {
+        $this->get('/blog')->assertSee('August 2018');
+    }
 }
