e7ac2828f3b837dffc2b2ec99320788daaabddc9
Bryce <bryceandy@rocketmail.com>
2018-08-08 13:39:21 +0300

    commit

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -185,4 +185,8 @@
         });
     </script>
 
+    <!-- Go to www.addthis.com/dashboard to customize your tools -->
+    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5b6ac1793196d3fc"></script>
+
+
 @endsection
