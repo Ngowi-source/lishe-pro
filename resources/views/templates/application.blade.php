631f38fd1b007ac6fdbfa95bb4d7f390347e634f
Bryce <bryceandy@rocketmail.com>
2018-08-10 01:46:58 +0300

    css

--- resources/views/templates/application.blade.php
+++ resources/views/templates/application.blade.php
@@ -30,7 +30,7 @@
                 <a href="/privacy">Privacy Policy</a>&nbsp;|&nbsp;<a href="/terms-of-service">Terms of Service</a>
             </div><br /><br />
             <div class="footer">
-                <span class="footblock">2018 &copy;Lishe Pro Tanzania. All Rights Reserved</span>
+                <span class="footblock">2018 &copy;Lishe Pro Tanzania</span>
                 <span class="footblock">Crafted by <a href="http://www.bryceandy.com">BryceAndy</a></span>
             </div>
         </div>
