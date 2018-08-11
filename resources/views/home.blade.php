3eca1c28eb02c43ec62f1470c3d2ea54b5ef10b1
Bryce <bryceandy@rocketmail.com>
2018-08-12 01:23:24 +0300

    css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -115,7 +115,7 @@
 
     <div id="homeWrapper">
         <div id="homeIntro">
-            <img src="{{'images/bg2-large.jpeg'}}"/>
+            <img src="{{--{{'images/bg2-large.jpeg'}}--}}https://www.pexels.com/photo/assorted-sliced-fruits-1128678/"/>
             <div id="intro" class="col-8 col-md-8 col-lg-6">
                 Better <span class="introline">Health</span> Through <span class="intro"><i>Nutrition </i></span><span class="intro2"><i>Tools</i></span>, Interactive Dietary <span class="intro">Assessment</span> and Expertise
             </div>
