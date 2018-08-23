b7c8869a51f65517cade1f28f2266e468592362c
Bryce <bryceandy@rocketmail.com>
2018-08-23 20:25:27 +0300

    css

--- resources/views/mails/accVer.blade.php
+++ resources/views/mails/accVer.blade.php
@@ -1,14 +1,14 @@
 
 
-Hello <b>{{ $user->firstname }}</b>,<br />
+Hello <b>{{ $user->firstname }}</b>,<br /><br />
 
-You have recently created an account with <i>Lishe Pro</i>, please click on the link below to verify your account.<br />
+You have recently created an account with <span style="color: #9BA747;"><i>Lishe Pro</i></span>, please click on the link below to verify your account.<br />
 
-<a href="https://lishep.herokuapp.com/verify/{{ base64_encode($user->id) }}">Verify Account</a><br /><br />
+<a style="text-decoration: none; color: #8B281F;" href="https://lishep.herokuapp.com/verify/{{ base64_encode($user->id) }}">Verify Account</a><br /><br />
 
 If you were not involved in this event please ignore this email. Do not reply to this email as messages are automatically
-sent.<br />
+sent.<br /><br />
 
-Regards,
+Regards,<br />
 Lishe Pro Team.
 
--- resources/views/mails/passRec.blade.php
+++ resources/views/mails/passRec.blade.php
@@ -1,14 +1,14 @@
 
 
-Hello <b>{{ $user->firstname }}</b>,<br />
+Hello <b>{{ $user->firstname }}</b>,<br /><br />
 
-You have requested to recover your account with Lishe Pro, please click on the link below to change your password.<br /><br />
+You have requested to recover your account with <span style="color: #9BA747;"><i>Lishe Pro</i></span>, please click on the link below to change your password.<br />
 
-<a href="https://lishep.herokuapp.com/pass-reset/{{ base64_encode($user->id) }}">Reset Password</a><br />
+<a style="text-decoration: none; color: #8B281F;" href="https://lishep.herokuapp.com/pass-reset/{{ base64_encode($user->id) }}">Reset Password</a><br /><br />
 
 If you were not involved in this event please ignore this email. Do not reply to this email as messages are automatically
-sent.<br />
+sent.<br /><br />
 
-Regards,
+Regards,<br />
 Lishe Pro Team.
 
