5fb45b5c28bcd1f4e6f34acdad0a80a93dcf9620
Bryce <bryceandy@rocketmail.com>
2018-09-04 20:26:43 +0300

    css

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -77,22 +77,22 @@
 
                 <span class="articleBod">
 
-                    {{--@if(strlen(preg_replace('/<p><img(.*)>/', '', $article->body))>400)
+                    @if(strlen(preg_replace('/<p><img(.*)>/', '', $article->body))>400)
                         {!! substr(preg_replace('/<p><img(.*)>/', '', $article->body),0,400) !!}... <span class="readMore"><a href="/blog/{{str_replace(' ','-',$article->title)}}"><i>Read More</i></a></span>
                     @else
                         {!! preg_replace('/<p><img(.*)>/', '', $articles->body) !!}
-                    @endif--}}
-
-                    @if(preg_match('/<p><img(.*)>/', $articles->body, $image))
-                            <img {{ $image[0]}} /> <br /> <span class="readMore"><a href="/blog/{{str_replace(' ','-',$article->title)}}"><i>Read More</i></a></span>
                     @endif
+
+                    {{--@if(preg_match('/<p><img(.*)>/', $articles->body, $image))
+                            <img {{ $image[0]}} /> <br /> <span class="readMore"><a href="/blog/{{str_replace(' ','-',$article->title)}}"><i>Read More</i></a></span>
+                    @endif--}}
 
                 </span><br />
                 <span class="articleTime"><a href="/blog/{{str_replace(' ','-',$article->title)}}"><i>{{count($article->comments)}} comments</i></a></span>
                 <hr/> <br />
             @endforeach
 
-            {{--{{$articles->links()}}--}}
+            {{$articles->links()}}
         </div>
 
     </div>
