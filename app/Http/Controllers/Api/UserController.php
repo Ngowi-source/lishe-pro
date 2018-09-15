0a7ce11e9fec315688a5c8e4768a869030a82b6a
Bryce <bryceandy@rocketmail.com>
2018-09-15 04:20:25 +0300

    resources

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
 {
     public function show($uid)
     {
-        $user = User::find($uid);
+        $user = User::findOrFail($uid);
 
         if(!$user)
         {
