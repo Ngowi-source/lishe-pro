5d893ee6e5b6c054c5aec0e85449c66b45c4960f
Bryce <bryceandy@rocketmail.com>
2018-09-24 14:24:42 +0300

    tags

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -131,7 +131,7 @@
 
             <h6><i class="fas fa-tags"></i> Tags</h6>
             @foreach($posted->tags as $tag)
-                <li><a href="/tags/{{$tag}}"> {{$tag}} </a></li>
+                <li><a href="/tags/{{$tag->name}}"> {{$tag->name}} </a></li>
             @endforeach
 
             <br><br />
