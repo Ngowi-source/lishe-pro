9eaa3345d51c8501510eb0b48c3f36a5f6bfd524
Bryce <bryceandy@rocketmail.com>
2018-12-03 19:10:05 +0300

    css and fixes

--- public/js/main.js
+++ public/js/main.js
@@ -20,7 +20,7 @@ var controller = new ScrollMagic.Controller();
 new ScrollMagic.Scene({
     triggerElement: "#header",
     triggerHook: "onLeave",
-    offset: -1
+    offset: 5
 })
     .setPin("#header")
     .addTo(controller);
