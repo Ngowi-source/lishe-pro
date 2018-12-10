ab6c0bc3dc5bf299f085fa1fa4f816a69c48a532
Bryce <bryceandy@rocketmail.com>
2018-12-10 22:45:19 +0300

    scss

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,7 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+    .sass('resources/assets/sass/shop.scss', 'public/css')
+    .combine('public/css/shop.css', 'public/css/app.css')
+    .minify('public/css/app.css');
