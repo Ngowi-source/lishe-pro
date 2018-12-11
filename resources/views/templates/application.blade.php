6c46bc371b47ea31dc503cce33f97dae1a98d8ab
Bryce <bryceandy@rocketmail.com>
2018-12-11 17:53:18 +0300

    css

--- resources/views/templates/application.blade.php
+++ resources/views/templates/application.blade.php
@@ -13,6 +13,7 @@
     <link rel="stylesheet" href="{{'css/app.min.css'}}">
     <link rel="stylesheet" href="{{'/css/app.min.css'}}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/noty/3.1.4/noty.min.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/noty/3.1.4/noty.js"></script>
