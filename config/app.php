101c3133d29220537ba09fc5c131c7f47ee683c8
bryceandy <bryceandy@rocketmail.com>
2019-01-08 19:43:11 +0300

    rename domain

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://lishep.herokuapp.com'),
+    'url' => env('APP_URL', 'https://chakula-pro.herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
