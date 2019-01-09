4d2994b64d9ffa00e3dd388cb13d2978baa6cf69
bryceandy <bryceandy@rocketmail.com>
2019-01-10 01:39:11 +0300

    Reply To unused

--- public/ipn.php
+++ public/ipn.php
@@ -36,7 +36,7 @@ use App\Transaction;
     $message .= '<b>Database update: </b>'.$dbupdated.'<br> ';
     $headers = 'From: ipntester@pesapal.com' . "\r\n";
     $headers .= 'MIME-Version: 1.0' . "\r\n";
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n".
     'Reply-To: bryzolildinho@gmail.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
