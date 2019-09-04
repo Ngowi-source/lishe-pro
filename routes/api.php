09c26fc6ddef41a2d16c3414e12f3f9c1ab55923
bryceandy <bryceandy@rocketmail.com>
2019-09-04 23:23:48 +0300

    Remove api routes

--- routes/api.php
+++ routes/api.php
@@ -11,16 +11,3 @@
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-
-Route::middleware('auth:api')->get('user/{uid}', 'Api\UserController@show');
-
-Route::middleware('auth:api')->get('users', 'Api\UserController@index');
-
-Route::middleware('auth:api')->delete('user/{uid}', 'Api\UserController@delete');
-
-Route::post('login', 'Api\AuthController@login');
-
-Route::post('register', 'Api\AuthController@register');
-
-Route::get('logout', 'Api\AuthController@logout');
